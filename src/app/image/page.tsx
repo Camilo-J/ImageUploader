449eb1794c79c08d6d5a6657adfab9178a970a11
Camilo-J <josecamilo1902@outlook.com>
2023-09-06 11:36:09 -0500

    feat: add logic to receive url from query params

--- src/app/image/page.tsx
+++ src/app/image/page.tsx
@@ -1,11 +1,21 @@
-import { ImageUploded } from "@/components/cardSucces/cardImage";
+import { ImageUploaded } from "@/components/cardSuccess/cardImage";
 import styles from "./page.module.css";
-export default async function CardContainer() {
-  await new Promise((resolve) => setTimeout(resolve, 5000));
+
+export default async function CardContainer({
+  searchParams,
+}: {
+  searchParams?: { [key: string]: string };
+}) {
+  if (!searchParams) {
+    return;
+  }
+  const data = searchParams.image;
+
+  await new Promise((resolve) => setTimeout(resolve, 4000));
 
   return (
     <section className={styles.mainContainer}>
-      <ImageUploded />
+      <ImageUploaded url={data} />
     </section>
   );
 }
