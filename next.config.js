c00a236384ba0e170f134a012ca7e5e472254f82
Camilo-J <josecamilo1902@outlook.com>
2025-11-28 19:23:22 -0500

    chore: update dependencies and configuration for Next.js and TypeScript

--- next.config.js
+++ next.config.js
@@ -1,12 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        hostname: process.env.FRONT_IMAGE_DOMAIN,
-      },
-    ],
-  },
+    remotePatterns: [{ hostname: process.env.FRONT_IMAGE_DOMAIN }],
+    unoptimized: true
+  }
 };
 
 module.exports = nextConfig;
